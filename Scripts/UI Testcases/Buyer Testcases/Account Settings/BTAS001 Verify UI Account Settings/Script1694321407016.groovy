2aaa094379c4b77ab1c3fbc2e8413c7067c65fcd
baothuy <truongbaothuy93@gmail.com>
2023-09-10 11:51:15 +0700

    add tcs BTAS001, BTCP001, BTCP002

---
+++ Scripts/UI Testcases/Buyer Testcases/Account Settings/BTAS001 Verify UI Account Settings/Script1694321407016.groovy
@@ -0,0 +1,94 @@
+import gocad.buyer.AccountSettingsPage
+import gocad.buyer.ChangePasswordPopup
+import gocad.common.LeftNavBar
+import gocad.common.MySignInPage
+import katalon.fw.lib.Page
+
+println '>> User buyer signs in to administration page'
+Page.nav(MySignInPage).enterCredentialAsBuyer().changeLanguage().clickSignIn().verifySuccessfullySignInAsBuyer()
+
+println '>> Click Settings nav menu'
+Page.nav(LeftNavBar).clickSettings()
+
+println '>> Verify UI account settings page'
+Page.nav(AccountSettingsPage).verifyUIVisible()
+
+println '>> Input contact information'
+Page.nav(AccountSettingsPage).inputFirstNameContact(contactChanged[0])
+							.inputLastNameContact(contactChanged[1])
+							.inputGenderContact(contactChanged[2])
+							.inputCompanyNameContact(contactChanged[3])
+							.inputPhoneContact(contactChanged[4])
+							.inputFaxContact(contactChanged[5])
+
+println '>> Input billing/shipping address information'
+Page.nav(AccountSettingsPage).inputFirstNameBillingAddress(billingAddressChanged[0])
+								.inputLastNameBillingAddress(billingAddressChanged[1])
+								.inputHouseNumberBillingAddress(billingAddressChanged[2])
+								.inputStreetBillingAddress(billingAddressChanged[3])
+								.selectStateBillingAddress(billingAddressChanged[4])
+								.inputZIPCodeBillingAddress(billingAddressChanged[5])
+								.inputCityBillingAddress(billingAddressChanged[6])
+								.inputFirstNameShippingAddress(shippingAddressChanged[0])
+								.inputLastNameShippingAddress(shippingAddressChanged[1])
+								.inputHouseNumberShippingAddress(shippingAddressChanged[2])
+								.inputStreetShippingAddress(shippingAddressChanged[3])
+								.selectStateShippingAddress(shippingAddressChanged[4])
+								.inputZIPCodeShippingAddress(shippingAddressChanged[5])
+								.inputCityShippingAddress(shippingAddressChanged[6])
+								.clickSaveChangesButton()
+								.verifyToastMessageWhenSaveChange()
+								
+Page.nav(AccountSettingsPage).sleep(1)
+								.refreshPage()
+								
+println '>> Verify contact information'
+Page.nav(AccountSettingsPage).verifyFirstNameContactValue(contactChanged[0])
+							.verifyLastNameContactValue(contactChanged[1])
+							.verifyGenderContactValue(contactChanged[2])
+							.verifyCompanyNameContactValue(contactChanged[3])
+							.verifyPhoneContactValue(contactChanged[4])
+							.verifyFaxContactValue(contactChanged[5])
+
+println '>> Verify billing/shipping address information'
+Page.nav(AccountSettingsPage).verifyFirstNameBillingAddressValue(billingAddressChanged[0])
+								.verifyLastNameBillingAddressValue(billingAddressChanged[1])
+								.verifyHouseNumberBillingAddressValue(billingAddressChanged[2])
+								.verifyStreetBillingAddressValue(billingAddressChanged[3])
+								.verifyStateBillingAddressValue(billingAddressChanged[4])
+								.verifyZIPCodeBillingAddressValue(billingAddressChanged[5])
+								.verifyCityBillingAddressValue(billingAddressChanged[6])
+								.verifyFirstNameShippingAddressValue(shippingAddressChanged[0])
+								.verifyLastNameShippingAddressValue(shippingAddressChanged[1])
+								.verifyHouseNumberShippingAddressValue(shippingAddressChanged[2])
+								.verifyStreetShippingAddressValue(shippingAddressChanged[3])
+								.verifyStateShippingAddressValue(shippingAddressChanged[4])
+								.verifyZIPCodeShippingAddressValue(shippingAddressChanged[5])
+								.verifyCityShippingAddressValue(shippingAddressChanged[6])
+								
+println '>> Input old data contact information'
+Page.nav(AccountSettingsPage).inputFirstNameContact(contact[0])
+							.inputLastNameContact(contact[1])
+							.inputGenderContact(contact[2])
+							.inputCompanyNameContact(contact[3])
+							.inputPhoneContact(contact[4])
+							.inputFaxContact(contact[5])
+							
+println '>> Input old data billing/shipping address information'
+Page.nav(AccountSettingsPage).inputFirstNameBillingAddress(billingAddress[0])
+								.inputLastNameBillingAddress(billingAddress[1])
+								.inputHouseNumberBillingAddress(billingAddress[2])
+								.inputStreetBillingAddress(billingAddress[3])
+								.selectStateBillingAddress(billingAddress[4])
+								.inputZIPCodeBillingAddress(billingAddress[5])
+								.inputCityBillingAddress(billingAddress[6])
+								.inputFirstNameShippingAddress(shippingAddress[0])
+								.inputLastNameShippingAddress(shippingAddress[1])
+								.inputHouseNumberShippingAddress(shippingAddress[2])
+								.inputStreetShippingAddress(shippingAddress[3])
+								.selectStateShippingAddress(shippingAddress[4])
+								.inputZIPCodeShippingAddress(shippingAddress[5])
+								.inputCityShippingAddress(shippingAddress[6])
+								.clickSaveChangesButton()
+								.verifyToastMessageWhenSaveChange()
+								.sleep(1)
