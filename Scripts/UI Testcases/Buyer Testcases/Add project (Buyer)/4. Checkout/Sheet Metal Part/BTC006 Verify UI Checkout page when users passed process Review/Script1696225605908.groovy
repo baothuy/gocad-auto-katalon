622487ad649734a2ebb190a7961b5c92840b2c0d
baothuy <truongbaothuy93@gmail.com>
2023-10-02 13:03:49 +0700

    add tcs checkout sheet metal

---
+++ Scripts/UI Testcases/Buyer Testcases/Add project (Buyer)/4. Checkout/Sheet Metal Part/BTC006 Verify UI Checkout page when users passed process Review/Script1696225605908.groovy
@@ -0,0 +1,77 @@
+import gocad.buyer.AccountSettingsPage
+import gocad.buyer.CheckoutPage
+import gocad.buyer.DraftPage
+import gocad.buyer.ReviewPage
+import gocad.common.AddProjectPopup
+import gocad.common.DataUploadPage
+import gocad.common.LeftNavBar
+import gocad.common.ManufacturingInformationPage
+import gocad.common.MySignInPage
+import gocad.common.SelectMaterialPopup
+import katalon.fw.lib.Page
+import katalon.utility.CommonUtility
+import katalon.utility.FileHelper
+
+
+println '>> FPA004 Verify buyer accept offer with offer calculated manually will confirm offer successfully'
+println '>> Random project name'
+def projectName = CommonUtility.generateRandomProjectName(10)
+
+println '>> User buyer signs in to administration page'
+Page.nav(MySignInPage).enterCredentialAsBuyer().changeLanguage().clickSignIn().verifySuccessfullySignInAsBuyer()
+
+println '>> Get company Name on Settings page'
+Page.nav(LeftNavBar).clickSettings()
+String companyName = Page.nav(AccountSettingsPage).getCompanyName()
+List<String> listBillingAddress = Page.nav(AccountSettingsPage).getBillingAddress()
+List<String> listShippingAddress = Page.nav(AccountSettingsPage).getShippingAddress()
+List<String> listCustomerInfo = Page.nav(AccountSettingsPage).getCustomerInfo()
+
+println '>> User buyer add project'
+Page.nav(LeftNavBar).clickAddProject()
+
+println '>> Open add project popup and add new project name'
+Page.nav(AddProjectPopup).inputProjectName("$projectName").clickOKButton()
+String projectId = Page.nav(DataUploadPage).getIdProject()
+println "projectId: $projectId"
+
+println '>>  Upload file part on Data upload page'
+Page.nav(DataUploadPage).uploadFileTesting('Sheet Metal Part', partName)
+
+println '>> Select material'
+Page.nav(ManufacturingInformationPage).clickPleaseSelectMaterial()
+Page.nav(SelectMaterialPopup).clickMaterialGroup(materialGroup).inputSearchMaterial(materialName)
+String material = Page.nav(SelectMaterialPopup).getMaterialAndNumber(materialName)
+println "material = $material"
+Page.nav(SelectMaterialPopup).selectMaterialName(materialName)
+
+println '>> Input required field'
+Page.nav(ManufacturingInformationPage).uploadFilePDFTesting('Sheet Metal Part', filePDF)
+										.clickProvideOwnMaterialCB(provideOwnProduct)
+										.inputThickness(partName, thicknessNum)
+										.inputQuantity(quantityNum)
+										.selectSurfaceTreatment(surfaceTreatment)
+										.selectRollingDirection(rollingDirection)
+										.clickDeburringCheckbox(deburring)
+										.inputCountersink(countersinkNum)
+										.inputThread(threadNum)
+										.inputComment(comment)
+
+println '>> click Calculate button'
+Page.nav(ManufacturingInformationPage).clickCalculate()
+									.clickContinueToOfferOverview()
+
+println '>> click checkout button'
+Page.nav(ReviewPage).clickCheckout()
+	
+println '>> Verify UI are visible'
+Page.nav(CheckoutPage).verifyUICheckoutVisible(partName)
+
+println '>> Verify file part can download successfully'
+Page.nav(CheckoutPage).clickFilePDFDownload()
+Page.nav(FileHelper).verifyFileDownloaded(projectName + ".pdf")
+
+println '>>  Clear data'
+Page.nav(LeftNavBar).clickDraft()
+Page.nav(DraftPage).clickArchiveAction(projectId)
+					.clickCloseToastMessage()
---
+++ Scripts/UI Testcases/Buyer Testcases/Add project (Buyer)/4. Checkout/Sheet Metal Part/BTC007 Verify can View information exactly the same with infor users input before/Script1696225736268.groovy
@@ -0,0 +1,86 @@
+import gocad.buyer.AccountSettingsPage
+import gocad.buyer.CheckoutPage
+import gocad.buyer.DraftPage
+import gocad.buyer.ReviewPage
+import gocad.common.AddProjectPopup
+import gocad.common.DataUploadPage
+import gocad.common.LeftNavBar
+import gocad.common.ManufacturingInformationPage
+import gocad.common.MySignInPage
+import gocad.common.SelectMaterialPopup
+import gocad.common.ViewPartPopup
+import katalon.fw.lib.Page
+import katalon.utility.CommonUtility
+
+
+
+println '>> FPA004 Verify buyer accept offer with offer calculated manually will confirm offer successfully'
+println '>> Random project name'
+def projectName = CommonUtility.generateRandomProjectName(10)
+
+println '>> User buyer signs in to administration page'
+Page.nav(MySignInPage).enterCredentialAsBuyer().changeLanguage().clickSignIn().verifySuccessfullySignInAsBuyer()
+
+println '>> Get company Name on Settings page'
+Page.nav(LeftNavBar).clickSettings()
+String companyName = Page.nav(AccountSettingsPage).getCompanyName()
+
+println '>> User buyer add project'
+Page.nav(LeftNavBar).clickAddProject()
+
+println '>> Open add project popup and add new project name'
+Page.nav(AddProjectPopup).inputProjectName("$projectName").clickOKButton()
+String projectId = Page.nav(DataUploadPage).getIdProject()
+println "projectId: $projectId"
+
+println '>>  Upload file part on Data upload page'
+Page.nav(DataUploadPage).uploadFileTesting('Sheet Metal Part', partName)
+
+println '>> Select material'
+Page.nav(ManufacturingInformationPage).clickPleaseSelectMaterial()
+Page.nav(SelectMaterialPopup).clickMaterialGroup(materialGroup).inputSearchMaterial(materialName)
+String material = Page.nav(SelectMaterialPopup).getMaterialAndNumber(materialName)
+println "material = $material"
+Page.nav(SelectMaterialPopup).selectMaterialName(materialName)
+
+println '>> Input required field'
+Page.nav(ManufacturingInformationPage).uploadFilePDFTesting('Sheet Metal Part', filePDF)
+										.clickProvideOwnMaterialCB(provideOwnProduct)
+										.inputThickness(partName, thicknessNum)
+										.inputQuantity(quantityNum)
+										.selectSurfaceTreatment(surfaceTreatment)
+										.selectRollingDirection(rollingDirection)
+										.clickDeburringCheckbox(deburring)
+										.inputCountersink(countersinkNum)
+										.inputThread(threadNum)
+										.inputComment(comment)
+
+println '>> click Calculate button'
+Page.nav(ManufacturingInformationPage).clickCalculate()
+									.clickContinueToOfferOverview()
+
+println '>> click checkout button'
+Page.nav(ReviewPage).clickCheckout()
+
+println '>> Verify information Address Information show correctly'
+println '>> Click View part information'
+Page.nav(CheckoutPage).clickMoreOption(partName)
+					  .clickView()
+					  
+println '>> Verify data on View page show correctly'
+Page.nav(ViewPartPopup).verifyMaterialValue(material)
+						  .verifyQuantityValue(quantityNum)
+						  .verifyThreadValue(threadNum)
+						  .verifyTolerancesNumberValue(tolerancesNum)
+						  .verifyTolerancesToggleValue(tolerancesToggle)
+						  .verifySurfaceTreatmentValue(surfaceTreatment)
+						  .verifySurfaceQualityValue(quality)
+						  .verifyAdditionalCommentsValue(comment)
+						  .verifyUnitPriceValue(unitPrice)
+						  .verifyNetPriceValue(netPrice)
+						  .clickClosePopup()
+						  
+println '>>  Clear data'
+Page.nav(LeftNavBar).clickDraft()
+Page.nav(DraftPage).clickArchiveAction(projectId)
+					.clickCloseToastMessage()
