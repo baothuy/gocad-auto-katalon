8cd09d1b8410b3240f437a6165b0700d34791370
baothuy <truongbaothuy93@gmail.com>
2023-10-23 10:52:38 +0700

    add new tcs customer overview

---
+++ Keywords/gocad/seller/CustomerOverviewPage.groovy
@@ -0,0 +1,53 @@
+package gocad.seller
+
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import katalon.fw.lib.BasePage
+
+
+
+
+public class CustomerOverviewPage extends BasePage<CustomerOverviewPage>{
+	
+	def rowOfCustomerId = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr")}
+	def customerIdCol = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr/td[1]")}
+	def fullNameCol = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr/td[2]")}
+	def partNumberCol = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr/td[3]")}
+	def orderTotalCol = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr/td[4]")}
+	def calculationCol = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr/td[5]")}
+	def discountCol = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr/td[6]")}
+	def registrationDateCol = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr/td[7]")}
+	def actionCol = { String customerId -> return xpath("//td[text()='$customerId']/parent::tr/td[8]/a")}
+	def row = { String row -> return "//*[@class='ant-table-tbody']/tr[$row]/"}
+	
+	public CustomerOverviewPage clickAction(String projectId) {
+		WebUI.click(actionCol(projectId))
+		return this
+	}
+	
+	public List<String> getDataRow(String rowNumber) {
+		String id = WebUI.getText(xpath(row(rowNumber) + "td[1]"))
+		String fullName = WebUI.getText(xpath(row(rowNumber) + "td[2]"))
+		String partNumber = WebUI.getText(xpath(row(rowNumber) + "td[3]"))
+		String orderTotal = WebUI.getText(xpath(row(rowNumber) + "td[4]"))
+		String calculation = WebUI.getText(xpath(row(rowNumber) + "td[5]"))
+		String discount = WebUI.getText(xpath(row(rowNumber) + "td[6]"))
+		String registrationDate = WebUI.getText(xpath(row(rowNumber) + "td[7]"))
+		List<String> dataRow = [id, fullName, partNumber, orderTotal, calculation, discount, registrationDate]
+		return dataRow
+	}
+	
+	public CustomerOverviewPage verifyUIVisible() {
+		WebUI.verifyElementVisible(xpath("//h3[text()='Customers']"))
+		WebUI.verifyElementVisible(xpath("//thead[@class='ant-table-thead']/tr/th[@aria-label='Id']"))
+		WebUI.verifyElementVisible(xpath("//thead[@class='ant-table-thead']/tr/th[@aria-label='Full Name']"))
+		WebUI.verifyElementVisible(xpath("//thead[@class='ant-table-thead']/tr/th[@aria-label='Parts (Number)']"))
+		WebUI.verifyElementVisible(xpath("//thead[@class='ant-table-thead']/tr/th[@aria-label='Order (Total)']"))
+		WebUI.verifyElementVisible(xpath("//thead[@class='ant-table-thead']/tr/th[@aria-label='Calculations (Number)']"))
+		WebUI.verifyElementVisible(xpath("//thead[@class='ant-table-thead']/tr/th[@aria-label='Discount']"))
+		WebUI.verifyElementVisible(xpath("//thead[@class='ant-table-thead']/tr/th[@aria-label='Registration Date']"))
+		WebUI.verifyElementVisible(xpath("//thead[@class='ant-table-thead']/tr/th[text()='Action']"))
+		WebUI.verifyElementVisible(xpath("//ul[contains(@class,'ant-table-pagination')]"))
+		return this
+	}
+}
---
+++ Scripts/UI Testcases/Seller Testcases/Customer Overview/STCOA001 Verify UI on Customer Overview page/Script1698029466118.groovy
@@ -0,0 +1,27 @@
+import gocad.common.LeftNavBar
+import gocad.common.MySignInPage
+import gocad.seller.CustomerOverviewDetailPage
+import gocad.seller.CustomerOverviewPage
+import katalon.fw.lib.Page
+
+println '>> User seller signs in page'
+Page.nav(MySignInPage).enterCredentialAsSeller().changeLanguage().clickSignIn().verifySuccessfullySignInAsSeller()
+
+println '>> Click search data project added at previous step'
+Page.nav(LeftNavBar).clickCustomerOverview()
+
+println '>> Verify UI Requested Offers page'
+Page.nav(CustomerOverviewPage).verifyUIVisible()
+
+println '>> Get data first row to go detail verify UI'
+List<String> dataRow = Page.nav(CustomerOverviewPage).getDataRow("1")
+String customerId = dataRow[0]
+
+Page.nav(CustomerOverviewPage).clickAction(customerId)
+									
+Page.nav(CustomerOverviewDetailPage).verifyUIVisible()
+									.verifyFullName(dataRow[1])
+									.verifyDiscount(dataRow[5])
+									.verifyPartNumber(dataRow[2])
+									.verifyOrderTotal(dataRow[3])
+									.verifyCalculationsNumber(dataRow[4])
