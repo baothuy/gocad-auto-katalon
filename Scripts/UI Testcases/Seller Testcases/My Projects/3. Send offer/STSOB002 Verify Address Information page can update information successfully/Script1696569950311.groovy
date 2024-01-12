0212191f0719490944b850e43ec338d28a75908e
baothuy <truongbaothuy93@gmail.com>
2024-01-12 20:53:19 +0700

    fix update address

--- Scripts/UI Testcases/Seller Testcases/My Projects/3. Send offer/STSOB002 Verify Address Information page can update information successfully/Script1696569950311.groovy
+++ Scripts/UI Testcases/Seller Testcases/My Projects/3. Send offer/STSOB002 Verify Address Information page can update information successfully/Script1696569950311.groovy
@@ -87,31 +87,35 @@ Page.nav(SendOfferPage).inputCustomer(email)
 println '>> Input edit information successfully'
 Page.nav(AddressInformationPopup).inputFirstNameBillingAddress(billingAddressChanged[0])
 								.inputLastNameBillingAddress(billingAddressChanged[1])
-								.inputHouseNumberBillingAddress(billingAddressChanged[2])
-								.inputStreetBillingAddress(billingAddressChanged[3])
-								.selectCountryBillingAddress(billingAddressChanged[7])
-								.selectStateBillingAddress(billingAddressChanged[4])
-								.inputZIPCodeBillingAddress(billingAddressChanged[5])
-								.selectCityBillingAddress(billingAddressChanged[6])							
+								.inputEmailBillingAddress(billingAddressChanged[2])
+								.inputPhoneBillingAddress(billingAddressChanged[3])
+								.inputStreetBillingAddress(billingAddressChanged[4])
+								.inputHouseNumberBillingAddress(billingAddressChanged[5])
+								.selectCountryBillingAddress(billingAddressChanged[6])
+								.selectStateBillingAddress(billingAddressChanged[7])								
+								.selectCityBillingAddress(billingAddressChanged[8])
+								.inputZIPCodeBillingAddress(billingAddressChanged[9])
 								.inputFirstNameShippingAddress(shippingAddressChanged[0])
 								.inputLastNameShippingAddress(shippingAddressChanged[1])
-								.inputHouseNumberShippingAddress(shippingAddressChanged[2])
-								.inputStreetShippingAddress(shippingAddressChanged[3])
-								.selectCountryShippingAddress(shippingAddressChanged[7])
-								.selectStateShippingAddress(shippingAddressChanged[4])
-								.inputZIPCodeShippingAddress(shippingAddressChanged[5])
-								.selectCityShippingAddress(shippingAddressChanged[6])								
+								.inputEmailShippingAddress(shippingAddressChanged[2])
+								.inputPhoneShippingAddress(shippingAddressChanged[3])
+								.inputStreetShippingAddress(shippingAddressChanged[4])
+								.inputHouseNumberShippingAddress(shippingAddressChanged[5])								
+								.selectCountryShippingAddress(shippingAddressChanged[6])
+								.selectStateShippingAddress(shippingAddressChanged[7])								
+								.selectCityShippingAddress(shippingAddressChanged[8])	
+								.inputZIPCodeShippingAddress(shippingAddressChanged[9])								
 
 Page.nav(SendOfferPage).clickSendOffer()
-List<String> listBillingAddress = [billingAddressChanged[0] + " " + billingAddressChanged[1], billingAddressChanged[2], billingAddressChanged[3], billingAddressChanged[4], billingAddressChanged[5], billingAddressChanged[6]]
-List<String> listShippingAddress = [shippingAddressChanged[0] + " " + shippingAddressChanged[1], shippingAddressChanged[2], shippingAddressChanged[3], shippingAddressChanged[4], shippingAddressChanged[5], shippingAddressChanged[6]]
+List<String> listBillingAddress = [billingAddressChanged[0] + " " + billingAddressChanged[1], billingAddressChanged[2], billingAddressChanged[3], billingAddressChanged[4], billingAddressChanged[5], billingAddressChanged[6], billingAddressChanged[7], billingAddressChanged[8], billingAddressChanged[9]]
+List<String> listShippingAddress = [shippingAddressChanged[0] + " " + shippingAddressChanged[1], shippingAddressChanged[2], shippingAddressChanged[3], shippingAddressChanged[4], shippingAddressChanged[5], shippingAddressChanged[6], shippingAddressChanged[7], shippingAddressChanged[8], shippingAddressChanged[9]]
 
 println '>> Verify after update show correctly on checkout page'
 Page.nav(CompletedCheckoutPage).clickBackToProject()
 Page.nav(DetailOffer).verifyBillingAddress(listBillingAddress)
 						.verifyShippingAddress(listShippingAddress)
 								
-println '>>  Clear data'
-Page.nav(LeftNavBar).clickMyProjects()
-Page.nav(MyProjectsPage).clickArchiveAction(projectId)
-					.clickCloseToastMessage()
+//println '>>  Clear data'
+//Page.nav(LeftNavBar).clickMyProjects()
+//Page.nav(MyProjectsPage).clickArchiveAction(projectId)
+//					.clickCloseToastMessage()
