591c5574df56e3777c26cf391b62e49ab5d1a33d
baothuy <truongbaothuy93@gmail.com>
2024-01-12 19:49:11 +0700

    fix missing change common step input MTP field

--- Scripts/UI Testcases/Buyer Testcases/Add project (Buyer)/4. Checkout/Sheet Metal Part/BTC007 Verify can View information exactly the same with infor users input before/Script1696225736268.groovy
+++ Scripts/UI Testcases/Buyer Testcases/Add project (Buyer)/4. Checkout/Sheet Metal Part/BTC007 Verify can View information exactly the same with infor users input before/Script1696225736268.groovy
@@ -44,17 +44,7 @@ println "material = $material"
 Page.nav(SelectMaterialPopup).selectMaterialName(materialName)
 
 println '>> Input required field'
-Page.nav(ManufacturingInformationPage).uploadFilePDFTesting('Sheet Metal Part', filePDF)
-										.clickProvideOwnMaterialCB(provideOwnProduct)
-										.selectThickness(partName, thicknessNum)
-										.inputQuantity(quantityNum)
-										.selectSurfaceTreatment(surfaceTreatment)
-										.selectLaserMarking(laserMarking)
-										.selectDeburring(deburring)
-										.inputCountersink(countersinkNum)
-										.inputThread(threadNum)
-										
-										.inputComment(comment)
+Page.nav(ManufacturingInformationPage).inputFieldMTPShop(provideOwnProduct, quantityNum, threadNum, tolerancesNum, tolerancesToggle, surfaceTreatment, quality, comment)
 
 println '>> click Calculate button'
 Page.nav(ManufacturingInformationPage).clickCalculate()
