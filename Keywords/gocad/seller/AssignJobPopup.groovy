b0a7c25829647cde4b85e295711c983fb62eb3d8
baothuy <truongbaothuy93@gmail.com>
2024-03-11 20:11:51 +0700

    change add job supplier

--- Keywords/gocad/seller/AssignJobPopup.groovy
+++ Keywords/gocad/seller/AssignJobPopup.groovy
@@ -20,41 +20,41 @@ public class AssignJobPopup extends BasePage<AssignJobPopup>{
 		WebUI.click(xpath("//span[text()='Cancel']/parent::button"))
 		return this
 	}
-
+	
 	public AssignJobPopup clickAcceptChange() {
 		WebUI.click(xpath("//*[@aria-label='check']/parent::button"))
 		return this
 	}
-
+	
 	public AssignJobPopup clickCloseChange() {
 		WebUI.click(xpath("//*[@aria-label='close']/parent::button"))
 		return this
 	}
-
+	
 	public AssignJobPopup selectAssignType(String type) {
 		//2 type supplier and supplier_group
 		WebUI.click(xpath("//*[@value='$type']/parent::span"))
 		return this
 	}
-
+	
 	public AssignJobPopup selectSupplierGroup(String supplierGroup) {
 		WebUI.click(xpath("//*[@value='supplier_group']/ancestor::div[@class='ant-space-item']//child::div[@class='ant-select-selector']"))
-		WebUI.click(xpath("//*[@id='rc_select_5_list']/following-sibling::div[@class='rc-virtual-list']//div[text()='$supplierGroup']"))
+		WebUI.click(xpath("//*[@id='supplierGroup_list']/following-sibling::div[@class='rc-virtual-list']//div[text()='$supplierGroup']"))
 		return this
 	}
-
+	
 	public AssignJobPopup selectSupplier(String supplier) {
 		WebUI.click(xpath("//*[@value='supplier']/ancestor::div[@class='ant-space-item']//child::div[@class='ant-select-selector']"))
-		WebUI.click(xpath("rc_select_6_list']/following-sibling::div[@class='rc-virtual-list']//div[text()='$supplier']"))
+		WebUI.click(xpath("//*[@id='supplier_list']/following-sibling::div[@class='rc-virtual-list']//div[text()='$supplier']"))
 		return this
 	}
-
+	
 	public AssignJobPopup inputDeliveryDate(String deliveryDate) {
 		WebUI.click(xpath("(//*[@class='ant-table-tbody']//span[@aria-label='edit'])[1]"))
 		WebUI.sendKeys(xpath("//input[@id='form-inline-adaptDeliveryDateForSupplier_adaptDeliveryDateForSupplier']"), deliveryDate + Keys.RETURN)
 		return this
 	}
-
+	
 	public AssignJobPopup inputSupplierPrice(String price) {
 		WebUI.click(xpath("(//*[@class='ant-table-tbody']//span[@aria-label='edit'])[2]"))
 		clearTextAndSendKeysByActions(xpath("//*[@id='form-inline-unitPrice']"), price)
--- Keywords/gocad/seller/OpenJobsPage.groovy
+++ Keywords/gocad/seller/OpenJobsPage.groovy
@@ -1,7 +1,10 @@
 package gocad.seller
 
+import org.openqa.selenium.Keys
+
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
+import gocad.common.ManufacturingInformationPage
 import katalon.fw.lib.BasePage
 
 
@@ -14,8 +17,67 @@ public class OpenJobsPage extends BasePage<OpenJobsPage>{
 		return this
 	}
 
+	public OpenJobsPage clickFirstJobsToAssign() {
+		WebUI.click(xpath("(//*[@class='ant-space-item']//input[@type='checkbox'])[1]"))
+		return this
+	}
+
 	public OpenJobsPage clickAssign() {
 		WebUI.click(xpath("//span[text()='Assign']/parent::button"))
+		return this
+	}
+
+	public OpenJobsPage inputDeliveryDate(String deliveryDate) {
+		WebUI.click(xpath("(//*[@class='ant-table-tbody']//span[@aria-label='edit'])[1]"))
+		WebUI.sendKeys(xpath("//input[@id='form-inline-adaptDeliveryDateForSupplier_adaptDeliveryDateForSupplier']"), deliveryDate + Keys.RETURN)
+		return this
+	}
+
+	public OpenJobsPage inputSupplierPrice(String price) {
+		WebUI.click(xpath("(//*[@class='ant-table-tbody']//span[@aria-label='edit'])[2]"))
+		clearTextAndSendKeysByActions(xpath("//*[@id='form-inline-unitPrice']"), price)
+		return this
+	}
+
+	public OpenJobsPage inputNoteToSupplier(String input) {
+		WebUI.click(xpath("(//*[text()='Note to supplier']/following::div[@class='textarea-inline']//span)[1]"))
+		WebUI.sendKeys(xpath("//*[@id='form-inline-sellerNote_sellerNote']"), input)
+		WebUI.click(xpath("//*[text()='Save']/parent::button"))
+		return this
+	}
+
+	public OpenJobsPage clickAcceptChange() {
+		WebUI.click(xpath("//*[@aria-label='check']/parent::button"))
+		return this
+	}
+
+	public OpenJobsPage clickCloseChange() {
+		WebUI.click(xpath("//*[@aria-label='close']/parent::button"))
+		return this
+	}
+
+	public OpenJobsPage clickViewProject(String projectName) {
+		WebUI.click(xpath("(//a[text()='$projectName'])[1]"))
+		return this
+	}
+
+	public OpenJobsPage verifyManufacturingInformationReportButtonVisible() {
+		WebUI.verifyElementVisible(xpath("(//span[text()=' Manufacturing information']/parent::button)[1]"))
+		return this
+	}
+
+	public OpenJobsPage verifyProcessingReportButtonVisible() {
+		WebUI.verifyElementVisible(xpath("(//span[text()=' Processing report']/parent::button)[1]"))
+		return this
+	}
+
+	public OpenJobsPage verifyEmissionReportButtonVisible() {
+		WebUI.verifyElementVisible(xpath("(//span[text()=' Emission report']/parent::button)[1]"))
+		return this
+	}
+
+	public OpenJobsPage clickDownloadImage() {
+		WebUI.click(xpath('(//a[@class="text-decoration-none"])[1]'))
 		return this
 	}
 }
